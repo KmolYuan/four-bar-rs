dfb16194eec5c56454c6c4ab69c2c9edc7c45814
KmolYuan <pyslvs@gmail.com>
2021-07-02 18:20:35 +0800

    Add deploy script.

--- start_server.sh
+++ start_server.sh
@@ -5,4 +5,4 @@ set -eu
 # which is the folder to where the web version is compiled.
 
 cargo install basic-http-server
-(cd docs && basic-http-server --addr 127.0.0.1:8080 .)
+cd docs && basic-http-server --addr 127.0.0.1:8080 .
