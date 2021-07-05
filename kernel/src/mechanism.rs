7dba481096a7aaffc2d59c8666b80a3b1b2d322b
KmolYuan <pyslvs@gmail.com>
2021-07-05 18:05:05 +0800

    Use trait to achieve zero copy.

--- kernel/src/mechanism.rs
+++ kernel/src/mechanism.rs
@@ -10,20 +10,16 @@ enum Formula {
 }
 
 impl Formula {
-    fn apply(&mut self, joints: &mut Vec<[f64; 2]>) {
+    fn apply(&mut self, joints: &mut Vec<impl Point>) {
         match self {
             Self::PLA(c1, d0, a0, t) => {
-                joints[*t] = Point::from(joints[*c1]).pla(*d0, *a0).to_slice();
+                joints[*t] = joints[*c1].pla(*d0, *a0);
             }
             Self::PLAP(c1, d0, a0, c2, t) => {
-                joints[*t] = Point::from(joints[*c1])
-                    .plap(*d0, *a0, &Point::from(joints[*c2]))
-                    .to_slice();
+                joints[*t] = joints[*c1].plap(*d0, *a0, &joints[*c2]);
             }
             Self::PLLP(c1, d0, d1, c2, inv, t) => {
-                joints[*t] = Point::from(joints[*c1])
-                    .pllp(*d0, *d1, &Point::from(joints[*c2]), *inv)
-                    .to_slice();
+                joints[*t] = joints[*c1].pllp(*d0, *d1, &joints[*c2], *inv);
             }
         }
     }
@@ -119,55 +115,66 @@ impl Mechanism {
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct Point {
-    pub x: f64,
-    pub y: f64,
-}
+/// A point-like memory layout to achieve zero copy.
+pub trait Point: Sized {
+    fn new(x: f64, y: f64) -> Self;
+    fn x(&self) -> f64;
+    fn y(&self) -> f64;
 
-impl From<[f64; 2]> for Point {
-    fn from(v: [f64; 2]) -> Self {
-        Self::new(v[0], v[1])
-    }
-}
-
-impl Point {
-    pub fn new(x: f64, y: f64) -> Self {
-        Self { x, y }
+    fn pla(&self, d0: f64, a0: f64) -> Self {
+        Self::new(self.x() + d0 * a0.cos(), self.y() + d0 * a0.sin())
     }
 
-    pub fn origin() -> Self {
-        Self::new(0., 0.)
+    fn plap(&self, d0: f64, a0: f64, rhs: &Self) -> Self {
+        let a1 = f64::atan2(rhs.y() - self.y(), rhs.x() - self.x()) + a0;
+        Self::new(self.x() + d0 * a1.cos(), self.y() + d0 * a1.sin())
     }
 
-    pub fn to_slice(&self) -> [f64; 2] {
-        [self.x, self.y]
-    }
-
-    pub fn pla(&self, d0: f64, a0: f64) -> Self {
-        Self::new(self.x + d0 * a0.cos(), self.y + d0 * a0.sin())
-    }
-
-    pub fn plap(&self, d0: f64, a0: f64, rhs: &Self) -> Self {
-        let a1 = f64::atan2(rhs.y - self.y, rhs.x - self.x) + a0;
-        Self::new(self.x + d0 * a1.cos(), self.y + d0 * a1.sin())
-    }
-
-    pub fn pllp(&self, d0: f64, d1: f64, rhs: &Self, inv: bool) -> Self {
-        let dx = rhs.x - self.x;
-        let dy = rhs.y - self.y;
-        let d = (self.x - rhs.x).hypot(self.y - rhs.y);
+    fn pllp(&self, d0: f64, d1: f64, rhs: &Self, inv: bool) -> Self {
+        let dx = rhs.x() - self.x();
+        let dy = rhs.y() - self.y();
+        let d = (self.x() - rhs.x()).hypot(self.y() - rhs.y());
         if d > d0 + d1 || d < (d0 - d1).abs() || (d < 1e-20 && d0 - d1 < 1e-20) {
             return Self::new(f64::NAN, f64::NAN);
         }
         let a = (d0 * d0 - d1 * d1 + d * d) / (2. * d);
         let h = (d0 * d0 - a * a).sqrt();
-        let xm = self.x + a * dx / d;
-        let ym = self.y + a * dy / d;
+        let xm = self.x() + a * dx / d;
+        let ym = self.y() + a * dy / d;
         if inv {
             Self::new(xm + h * dy / d, ym - h * dx / d)
         } else {
             Self::new(xm - h * dy / d, ym + h * dx / d)
         }
+    }
+}
+
+impl Point for [f64; 2] {
+    #[inline(always)]
+    fn new(x: f64, y: f64) -> Self {
+        [x, y]
+    }
+    #[inline(always)]
+    fn x(&self) -> f64 {
+        self[0]
+    }
+    #[inline(always)]
+    fn y(&self) -> f64 {
+        self[1]
+    }
+}
+
+impl Point for (f64, f64) {
+    #[inline(always)]
+    fn new(x: f64, y: f64) -> Self {
+        (x, y)
+    }
+    #[inline(always)]
+    fn x(&self) -> f64 {
+        self.0
+    }
+    #[inline(always)]
+    fn y(&self) -> f64 {
+        self.1
     }
 }
