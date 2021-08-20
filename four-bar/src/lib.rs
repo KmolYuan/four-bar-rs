54ae0927bcf3ebddf4aa6734f77efb3da18e6e20
KmolYuan <pyslvs@gmail.com>
2021-08-20 16:44:04 +0800

    Doc.rs settings.

--- four-bar/src/lib.rs
+++ four-bar/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(doc_cfg, feature(doc_cfg))]
 pub use crate::anti_sym_ext::*;
 pub use crate::mechanism::*;
 pub use crate::point::*;
@@ -6,6 +7,7 @@ mod anti_sym_ext;
 mod mechanism;
 mod point;
 #[cfg(feature = "synthesis")]
+#[cfg_attr(doc_cfg, doc(cfg(feature = "synthesis")))]
 pub mod synthesis;
 #[cfg(test)]
 mod tests;
