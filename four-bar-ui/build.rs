f47d89be8d26bd87789c9c8b9e4e34b6b7374c12
KmolYuan <pyslvs@gmail.com>
2021-10-09 18:32:59 +0800

    Auto generate favicon.

---
+++ four-bar-ui/build.rs
@@ -0,0 +1,29 @@
+fn main() {
+    #[cfg(not(target_arch = "wasm32"))]
+    make_favicon();
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn make_favicon() {
+    use image::{load_from_memory_with_format, DynamicImage, ImageFormat};
+    use std::fs::write;
+    const ICON: &[u8] = include_bytes!("../docs/favicon.png");
+    let buf = if let DynamicImage::ImageRgba8(buf) =
+        load_from_memory_with_format(ICON, ImageFormat::Png).unwrap()
+    {
+        buf
+    } else {
+        panic!("Never failed");
+    };
+    let doc = format!(
+        "\
+pub const WIDTH: u32 = {};
+pub const HEIGHT: u32 = {};
+pub const ICON: &[u8] = &{:?};
+",
+        buf.width(),
+        buf.height(),
+        buf.to_vec()
+    );
+    write("./src/icon.rs", doc).unwrap();
+}
