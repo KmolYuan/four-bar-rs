6917107a436cfa2af366b57aab99355ab1f1c145
KmolYuan <pyslvs@gmail.com>
2021-10-10 17:26:37 +0800

    Optimize imports.

--- four-bar/src/lib.rs
+++ four-bar/src/lib.rs
@@ -1,10 +1,7 @@
 //! Four🍀bar is a simulator, a synthesizing tool for four-bar linkage mechanism.
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 #![warn(missing_docs)]
-pub use crate::anti_sym_ext::*;
-pub use crate::four_bar::*;
-pub use crate::mechanism::*;
-pub use crate::point::*;
+pub use crate::{anti_sym_ext::*, four_bar::*, mechanism::*, point::*};
 
 mod anti_sym_ext;
 mod four_bar;
