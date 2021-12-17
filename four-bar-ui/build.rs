19dad26d48991cf730132f65aa60c8586b11ebed
KmolYuan <pyslvs@gmail.com>
2021-12-17 21:20:34 +0800

    Add Windows icon.

--- four-bar-ui/build.rs
+++ four-bar-ui/build.rs
@@ -1,14 +1,11 @@
-#[cfg(not(target_arch = "wasm32"))]
-use {
-    image::{io::Reader, GenericImageView},
-    std::{error::Error, fs::write, path::PathBuf},
-};
-
 #[cfg(target_arch = "wasm32")]
 fn main() {}
 
 #[cfg(not(target_arch = "wasm32"))]
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use image::{io::Reader, GenericImageView};
+    use std::{fs::write, path::PathBuf};
+
     println!("cargo:rerun-if-changed=src/assets/favicon.png");
     let out_dir = PathBuf::from(std::env::var("OUT_DIR")?);
     let img = Reader::open("src/assets/favicon.png")?.decode()?;
@@ -22,5 +19,13 @@ pub const ICON: &[u8] = &{:?};",
         img.as_bytes()
     );
     write(out_dir.join("icon.rs"), doc)?;
+    #[cfg(windows)]
+    {
+        let ico_path = out_dir.join("icon.ico");
+        img.save(&ico_path)?;
+        let win_rc = out_dir.join("resource.rc");
+        write(&win_rc, format!("four_bar ICON \"{:?}\"\r\n", ico_path))?;
+        embed_resource::compile(win_rc);
+    }
     Ok(())
 }
