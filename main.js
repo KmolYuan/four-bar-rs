5ea7c94e5e2f8ef06fe858c35c83b0b1fe24a1e5
KmolYuan <pyslvs@gmail.com>
2021-12-26 03:39:38 +0000

    Deploying to gh-pages from  @ 13f5ba0ddf93619cbc7e377e6ec01714273a523f 🚀

---
+++ main.js
@@ -0,0 +1,47 @@
+import * as wasm from "./pkg/four_bar_ui.js";
+
+// Module level references
+const a = document.createElement("a");
+const input = document.createElement("input");
+input.type = "file";
+const reader = new FileReader();
+input.addEventListener("change", function () {
+    if (this.files.length === 0)
+        return;
+    reader.readAsText(this.files[0]);
+});
+
+// Utility functions
+window.save_file = function (s, file_name) {
+    a.download = file_name;
+    a.href = window.URL.createObjectURL(new Blob([s], {type: "application/octet-stream"}));
+    a.click();
+};
+window.load_file = function (format, done) {
+    reader.onload = e => done(e.target.result);
+    input.accept = format;
+    input.click();
+};
+window.get_link = function () {
+    return location.href;
+};
+window.login = function (account, body, done) {
+    fetch(location.href + "login/" + account, {
+        method: "POST",
+        body: body,
+        headers: {"content-type": "application/json"},
+        mode: "cors",
+    }).then(res => done(res.ok));
+};
+window.logout = function (done) {
+    fetch(location.href + "logout", {
+        method: "POST",
+        mode: "cors",
+    }).then(res => done(res.ok));
+};
+
+// Startup WebAssembly
+(async () => {
+    await wasm.default();
+    await wasm.start("main_canvas");
+})();
