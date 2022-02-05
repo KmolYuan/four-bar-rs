a96b9902731a57733d5a42a72a6d36c8287508bd
KmolYuan <pyslvs@gmail.com>
2022-02-05 12:00:54 +0800

    Update dependencies.

--- four-bar-ui/build.rs
+++ four-bar-ui/build.rs
@@ -3,12 +3,9 @@ fn main() {}
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    use image::{io::Reader, GenericImageView};
-    use std::{fs::write, path::PathBuf};
-
     println!("cargo:rerun-if-changed=src/assets/favicon.png");
-    let out_dir = PathBuf::from(std::env::var("OUT_DIR")?);
-    let img = Reader::open("src/assets/favicon.png")?.decode()?;
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR")?);
+    let img = image::io::Reader::open("src/assets/favicon.png")?.decode()?;
     let doc = format!(
         "\
 pub const WIDTH: u32 = {};
@@ -18,7 +15,7 @@ pub const ICON: &[u8] = &{:?};",
         img.height(),
         img.as_bytes()
     );
-    write(out_dir.join("icon.rs"), doc)?;
+    std::fs::write(out_dir.join("icon.rs"), doc)?;
     #[cfg(windows)]
     {
         let ico_path = out_dir.join("icon.ico");
