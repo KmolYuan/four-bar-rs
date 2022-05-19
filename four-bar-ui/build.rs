562d598509460616400249777bdbc49277ce9b6c
KmolYuan <pyslvs@gmail.com>
2022-05-19 13:08:02 +0800

    Update `build.rs`.

--- four-bar-ui/build.rs
+++ four-bar-ui/build.rs
@@ -1,7 +1,3 @@
-#[cfg(target_arch = "wasm32")]
-fn main() {}
-
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     println!("cargo:rerun-if-changed=src/assets/*");
     let out = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
@@ -18,11 +14,14 @@ pub const ICON: &[u8] = &{:?};",
     std::fs::write(out.join("icon.rs"), doc).unwrap();
     #[cfg(windows)]
     {
-        let ico_path = out.join("icon.ico");
-        ico.save(&ico_path).unwrap();
-        winres::WindowsResource::new()
-            .set_icon(ico_path.to_str().unwrap())
-            .compile()
-            .unwrap();
+        let target_env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
+        if target_env == "gnu" || target_env == "msvc" {
+            let ico_path = out.join("icon.ico");
+            ico.save(&ico_path).unwrap();
+            winres::WindowsResource::new()
+                .set_icon(ico_path.to_str().unwrap())
+                .compile()
+                .unwrap();
+        }
     }
 }
