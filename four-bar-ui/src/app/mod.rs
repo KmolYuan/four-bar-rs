77ddbf816c4b8c225c8d3b0b52b2e39fd19b80a9
KmolYuan <pyslvs@gmail.com>
2022-08-22 23:16:56 +0800

    Support mobile view.

--- four-bar-ui/src/app/mod.rs
+++ four-bar-ui/src/app/mod.rs
@@ -17,6 +17,16 @@ const FONT: &[(&str, &[u8])] = &[
     ("emoji", include_bytes!("../../assets/emoji-icon-font.ttf")),
 ];
 
+fn side_panel(ctx: &Context, f: impl FnOnce(&mut Ui)) {
+    SidePanel::left("side panel")
+        .resizable(false)
+        .show(ctx, |ui| ScrollArea::vertical().show(ui, f));
+}
+
+fn pan_panel(ui: &mut Ui, f: impl FnOnce(&mut Ui)) {
+    ScrollArea::vertical().show(ui, f);
+}
+
 #[derive(Default, Deserialize, Serialize, PartialEq)]
 enum Panel {
     #[default]
@@ -88,6 +98,11 @@ impl App {
             .show(ctx, |ui| {
                 ui.label(concat!["Version: v", env!("CARGO_PKG_VERSION")]);
                 ui.label(env!("CARGO_PKG_DESCRIPTION"));
+                ui.horizontal(|ui| {
+                    url_btn(ui, "", "Repository", env!("CARGO_PKG_REPOSITORY"));
+                    url_btn(ui, "⮋", "Release", RELEASE_URL);
+                });
+                ui.hyperlink_to("Powered by egui", "https://github.com/emilk/egui/");
                 ui.separator();
                 ui.heading("Author");
                 ui.label(env!("CARGO_PKG_AUTHORS"));
@@ -108,25 +123,46 @@ impl App {
         ui.selectable_value(&mut self.panel, Panel::Options, "🛠")
             .on_hover_text("Options");
         ui.selectable_value(&mut self.panel, Panel::Off, "⛶")
-            .on_hover_text("Close Panel");
+            .on_hover_text("Focus on Canvas");
         ui.with_layout(Layout::right_to_left(Align::LEFT), |ui| {
             let style = ui.style().clone();
             if let Some(v) = style.visuals.light_dark_small_toggle_button(ui) {
                 ui.ctx().set_visuals(v);
             }
-            url_btn(ui, "⮋", "Release", RELEASE_URL);
-            url_btn(ui, "", "Repository", env!("CARGO_PKG_REPOSITORY"));
             if ui.small_button("💁").on_hover_text("Welcome").clicked() {
                 self.welcome_off = !self.welcome_off;
             }
-            ui.hyperlink_to("Powered by egui", "https://github.com/emilk/egui/");
         });
     }
 
-    fn side_panel(ctx: &Context, f: impl FnOnce(&mut Ui)) {
-        SidePanel::left("side panel")
-            .resizable(false)
-            .show(ctx, |ui| ScrollArea::vertical().show(ui, f));
+    fn canvas(&mut self, ui: &mut Ui) {
+        plot::Plot::new("canvas")
+            .data_aspect(1.)
+            .legend(Default::default())
+            .coordinates_formatter(plot::Corner::LeftBottom, Default::default())
+            .show(ui, |ui| {
+                self.linkage.plot(ui);
+                self.syn.plot(ui);
+            });
+    }
+
+    fn mobile_view(&mut self, ctx: &Context) {
+        CentralPanel::default().show(ctx, |ui| match self.panel {
+            Panel::Linkages => pan_panel(ui, |ui| self.linkage.show(ui)),
+            Panel::Synthesis => pan_panel(ui, |ui| self.syn.show(ui, &mut self.linkage)),
+            Panel::Options => pan_panel(ui, |ui| self.linkage.option(ui)),
+            Panel::Off => self.canvas(ui),
+        });
+    }
+
+    fn pc_view(&mut self, ctx: &Context) {
+        match self.panel {
+            Panel::Linkages => side_panel(ctx, |ui| self.linkage.show(ui)),
+            Panel::Synthesis => side_panel(ctx, |ui| self.syn.show(ui, &mut self.linkage)),
+            Panel::Options => side_panel(ctx, |ui| self.linkage.option(ui)),
+            Panel::Off => (),
+        }
+        CentralPanel::default().show(ctx, |ui| self.canvas(ui));
     }
 }
 
@@ -134,22 +170,11 @@ impl eframe::App for App {
     fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
         self.welcome(ctx);
         TopBottomPanel::top("menu").show(ctx, |ui| ui.horizontal(|ui| self.menu(ui)));
-        match self.panel {
-            Panel::Linkages => Self::side_panel(ctx, |ui| self.linkage.show(ui)),
-            Panel::Synthesis => Self::side_panel(ctx, |ui| self.syn.show(ui, &mut self.linkage)),
-            Panel::Options => Self::side_panel(ctx, |ui| self.linkage.option(ui)),
-            Panel::Off => (),
+        if ctx.input().screen_rect.width() < 600. {
+            self.mobile_view(ctx);
+        } else {
+            self.pc_view(ctx);
         }
-        CentralPanel::default().show(ctx, |ui| {
-            plot::Plot::new("canvas")
-                .data_aspect(1.)
-                .legend(Default::default())
-                .coordinates_formatter(plot::Corner::LeftBottom, Default::default())
-                .show(ui, |ui| {
-                    self.linkage.plot(ui);
-                    self.syn.plot(ui);
-                });
-        });
     }
 
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
