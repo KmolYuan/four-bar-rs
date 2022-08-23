0a09b4906509890ff9d84cb067456da27d62e374
KmolYuan <pyslvs@gmail.com>
2022-08-23 09:38:39 +0800

    Fix executable name.

--- four-bar-ui/src/main.rs
+++ four-bar-ui/src/main.rs
@@ -12,19 +12,21 @@ fn main() {
         /// File path
         files: Vec<String>,
     }
-    const ICON: &[u8] = include_bytes!("../assets/favicon.png");
-    let icon = image::load_from_memory_with_format(ICON, ImageFormat::Png).unwrap();
-    let opt = eframe::NativeOptions {
-        icon_data: Some(eframe::IconData {
-            width: icon.width(),
-            height: icon.height(),
-            rgba: icon.into_bytes(),
-        }),
-        ..Default::default()
+    let files = Entry::parse().files;
+    let opt = {
+        const ICON: &[u8] = include_bytes!("../assets/favicon.png");
+        let icon = image::load_from_memory_with_format(ICON, ImageFormat::Png).unwrap();
+        eframe::NativeOptions {
+            icon_data: Some(eframe::IconData {
+                width: icon.width(),
+                height: icon.height(),
+                rgba: icon.into_bytes(),
+            }),
+            ..Default::default()
+        }
     };
     #[cfg(windows)]
     let _ = unsafe { winapi::um::wincon::FreeConsole() };
-    let files = Entry::parse().files;
     eframe::run_native("Four-bar", opt, Box::new(|ctx| App::new(ctx, files)))
 }
 
