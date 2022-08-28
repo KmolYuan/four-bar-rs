885c31e4d79db95c1ad230dcdf1e513c831400ef
KmolYuan <pyslvs@gmail.com>
2022-08-28 13:36:10 +0800

    Split out to type.

--- four-bar-codebook/src/lib.rs
+++ four-bar-codebook/src/lib.rs
@@ -17,51 +17,66 @@ pub struct Opt {
     pub open: bool,
 }
 
-/// Generate and write a codebook for close curve mechanism.
-pub fn write<W1, W2>(fb_w: W1, efd_w: W2, opt: Opt) -> Result<(), WriteNpyError>
-where
-    W1: std::io::Write,
-    W2: std::io::Write,
-{
-    let Opt { n, res, harmonic, open } = opt;
-    let rng = Rng::new(None);
-    let fb_stack = Mutex::new(Vec::with_capacity(n));
-    let stack = Mutex::new(Vec::with_capacity(n));
-    loop {
-        let len = stack.lock().unwrap().len();
-        (0..(n - len) / 2).into_par_iter().for_each(|_| {
-            let v = [
-                rng.float(1e-4..10.),
-                rng.float(1e-4..10.),
-                rng.float(1e-4..10.),
-                rng.float(1e-4..10.),
-                rng.float(0.0..TAU),
-            ];
-            [false, true].into_par_iter().for_each(|inv| {
-                let fb = NormFourBar::from_vec(v, inv).to_close_curve();
-                let fb = match open {
-                    false => fb.to_close_curve(),
-                    true => fb.to_open_curve(),
-                };
-                if open != fb.ty().is_open_curve() {
-                    return;
-                }
-                if let Some([start, end]) = fb.angle_bound() {
-                    let curve = Mechanism::new(&fb).curve(start, end, res);
-                    let efd = Efd2::from_curve(&curve, harmonic);
-                    fb_stack.lock().unwrap().push(arr1(&fb.v));
-                    stack.lock().unwrap().push(efd.unwrap());
-                }
-            })
-        });
-        if stack.lock().unwrap().len() >= n {
-            break;
+/// Codebook type.
+pub struct CodeBook {
+    fb: Array2<f64>,
+    efd: Array3<f64>,
+}
+
+impl CodeBook {
+    /// Generate codebook data.
+    pub fn generate(opt: Opt) -> Self {
+        let Opt { n, res, harmonic, open } = opt;
+        let rng = Rng::new(None);
+        let fb_stack = Mutex::new(Vec::with_capacity(n));
+        let efd_stack = Mutex::new(Vec::with_capacity(n));
+        loop {
+            let len = efd_stack.lock().unwrap().len();
+            (0..(n - len) / 2).into_par_iter().for_each(|_| {
+                let v = [
+                    rng.float(1e-4..10.),
+                    rng.float(1e-4..10.),
+                    rng.float(1e-4..10.),
+                    rng.float(1e-4..10.),
+                    rng.float(0.0..TAU),
+                ];
+                [false, true].into_par_iter().for_each(|inv| {
+                    let fb = NormFourBar::from_vec(v, inv).to_close_curve();
+                    let fb = match open {
+                        false => fb.to_close_curve(),
+                        true => fb.to_open_curve(),
+                    };
+                    if open != fb.ty().is_open_curve() {
+                        return;
+                    }
+                    if let Some([start, end]) = fb.angle_bound() {
+                        let curve = Mechanism::new(&fb).curve(start, end, res);
+                        let efd = Efd2::from_curve(&curve, harmonic);
+                        fb_stack.lock().unwrap().push(arr1(&fb.v));
+                        efd_stack.lock().unwrap().push(efd.unwrap());
+                    }
+                })
+            });
+            if efd_stack.lock().unwrap().len() >= n {
+                break;
+            }
         }
+        let fb = fb_stack.into_inner().unwrap();
+        let efd = efd_stack.into_inner().unwrap();
+        let arrays = fb.iter().take(n).map(Array::view).collect::<Vec<_>>();
+        let fb = ndarray::stack(Axis(0), &arrays).unwrap();
+        let arrays = efd.iter().take(n).map(Array::view).collect::<Vec<_>>();
+        let efd = ndarray::stack(Axis(0), &arrays).unwrap();
+        Self { fb, efd }
     }
-    let fb_stack = fb_stack.into_inner().unwrap();
-    let stack = stack.into_inner().unwrap();
-    let arrays = fb_stack.iter().take(n).map(Array::view).collect::<Vec<_>>();
-    ndarray::stack(Axis(0), &arrays).unwrap().write_npy(fb_w)?;
-    let arrays = stack.iter().take(n).map(Array::view).collect::<Vec<_>>();
-    ndarray::stack(Axis(0), &arrays).unwrap().write_npy(efd_w)
+
+    /// Write codebook to NPY files.
+    pub fn write<W1, W2>(&self, fb_w: W1, efd_w: W2) -> Result<(), WriteNpyError>
+    where
+        W1: std::io::Write,
+        W2: std::io::Write,
+    {
+        self.fb.write_npy(fb_w)?;
+        self.efd.write_npy(efd_w)
+    }
 }
