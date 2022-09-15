96da87e0de34a5be6fe498681d775426a3110e80
KmolYuan <pyslvs@gmail.com>
2022-09-15 17:58:51 +0800

    Simplify `build.rs`.

--- four-bar-ui/build.rs
+++ four-bar-ui/build.rs
@@ -6,7 +6,6 @@ fn main() {
     let profile = std::env::var("PROFILE").unwrap();
     let target_env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
     if target_env == "gnu" || target_env == "msvc" && profile == "release" {
-        println!("cargo:rerun-if-changed=assets/*");
         let out = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
         let ico = image::open("assets/favicon.png").unwrap();
         let ico_path = out.join("icon.ico");
