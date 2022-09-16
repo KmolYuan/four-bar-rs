58f031a6a80f6ebe7380d121f72d76f8e66567a3
KmolYuan <pyslvs@gmail.com>
2022-09-16 04:02:55 +0000

    Deploying to gh-pages from @ KmolYuan/four-bar-rs@1bd795cb1fb68eade7ed05cbd8c177ec1042ea30 🚀

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ window.loading_finished = () => document.getElementById("loading-text").remove()
 window.open_file = (ext, done, multiple) => {
     const input = document.createElement("input");
     input.type = "file";
+    input.accept = ext;
     input.multiple = multiple;
-    input.onchange = () => Array.from(input.files).forEach(file => {
+    input.onchange = () => [...input.files].forEach(file => {
         const reader = new FileReader();
         reader.onload = () => done(file.name, reader.result);
         reader.readAsText(file);
     });
-    input.accept = ext;
     input.click();
 };
 window.save_file = (s, path) => {
