1e07a55804d206afd084035d0538dd1e55ecd126
KmolYuan <pyslvs@gmail.com>
2022-11-05 23:12:38 +0800

    Use self path in root.

--- four-bar-ui/src/main.rs
+++ four-bar-ui/src/main.rs
@@ -8,14 +8,15 @@ mod syn_method;
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    cli::Entry::parse();
+    self::cli::Entry::parse();
 }
 
 #[cfg(target_arch = "wasm32")]
 fn main() {
+    use self::app::App;
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default();
     let opt = eframe::WebOptions::default();
-    eframe::start_web("app", opt, Box::new(|ctx| app::App::new(ctx, Vec::new())))
+    eframe::start_web("app", opt, Box::new(|ctx| App::new(ctx, Vec::new())))
         .expect("failed to startup");
 }
