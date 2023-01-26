25bf77a92790b7da3aef8fef2b896a29b50d08a1
KmolYuan <pyslvs@gmail.com>
2023-01-26 18:30:34 +0800

    Add `inv` option.

--- four-bar/src/cb/vectorize.rs
+++ four-bar/src/cb/vectorize.rs
@@ -3,12 +3,12 @@ use crate::*;
 macro_rules! impl_vec {
     ($($ty:ty{$num:literal}),+) => {$(
         impl Vectorize<$num> for $ty {
-            fn from_array(v: [f64; $num]) -> Self {
-                Self::new(v, false)
+            fn from_array(v: [f64; $num], inv: bool) -> Self {
+                Self::new(v, inv)
             }
 
-            fn to_array(self) -> [f64; $num] {
-                self.as_array()
+            fn to_array(self) -> (bool, [f64; $num]) {
+                (self.inv(), self.as_array())
             }
         }
     )+};
@@ -20,13 +20,13 @@ pub trait Vectorize<const N: usize>: Sized {
     const N: usize = N;
 
     /// Convert from array.
-    fn from_array(v: [f64; N]) -> Self;
+    fn from_array(v: [f64; N], inv: bool) -> Self;
     /// Convert to array.
-    fn to_array(self) -> [f64; N];
+    fn to_array(self) -> (bool, [f64; N]);
 
     /// Convert from unknown-size slice.
-    fn from_slice(v: &[f64]) -> Result<Self, std::array::TryFromSliceError> {
-        Ok(Self::from_array(v.try_into()?))
+    fn from_slice(v: &[f64], inv: bool) -> Result<Self, std::array::TryFromSliceError> {
+        Ok(Self::from_array(v.try_into()?, inv))
     }
 }
 
