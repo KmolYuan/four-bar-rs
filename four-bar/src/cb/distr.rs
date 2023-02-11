c865ae3b9267ce3ff993b32b8ee61bd13744f956
KmolYuan <pyslvs@gmail.com>
2023-02-11 11:54:33 +0800

    Add spherical part.

--- four-bar/src/cb/distr.rs
+++ four-bar/src/cb/distr.rs
@@ -1,14 +1,28 @@
 use crate::*;
+use mh::rand::{Distribution, Rng};
 
 /// Uniform distribution of the [`NormFourBar`] type.
 pub struct NormFbDistr;
 
-impl mh::rand::Distribution<[NormFourBar; 2]> for NormFbDistr {
-    fn sample<R: mh::rand::Rng + ?Sized>(&self, rng: &mut R) -> [NormFourBar; 2] {
+impl Distribution<[NormFourBar; 2]> for NormFbDistr {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> [NormFourBar; 2] {
         let v = crate::planar_syn::BOUND[..5]
             .iter()
             .map(|&[u, l]| rng.gen_range(u..l))
             .collect::<Vec<_>>();
         [false, true].map(|inv| NormFourBar::try_from(v.as_slice()).unwrap().with_inv(inv))
+    }
+}
+
+/// Uniform distribution of the [`SNormFourBar`] type.
+pub struct SNormFbDistr;
+
+impl Distribution<[SNormFourBar; 2]> for SNormFbDistr {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> [SNormFourBar; 2] {
+        let v = crate::spherical_syn::BOUND[..6]
+            .iter()
+            .map(|&[u, l]| rng.gen_range(u..l))
+            .collect::<Vec<_>>();
+        [false, true].map(|inv| SNormFourBar::try_from(v.as_slice()).unwrap().with_inv(inv))
     }
 }
--- four-bar/src/spherical_syn.rs
+++ four-bar/src/spherical_syn.rs
@@ -1,1 +1,16 @@
 //! The synthesis implementation of spherical four-bar linkage mechanisms.
+use std::f64::consts::{FRAC_PI_8, TAU};
+
+/// The minimum input angle bound. (π/16)
+pub const MIN_ANGLE: f64 = FRAC_PI_8 * 0.5;
+/// Boundary of the objective variables.
+pub const BOUND: [[f64; 2]; 8] = [
+    [0., TAU],
+    [0., TAU],
+    [0., TAU],
+    [0., TAU],
+    [0., TAU],
+    [0., TAU],
+    [0., TAU],
+    [0., TAU],
+];
