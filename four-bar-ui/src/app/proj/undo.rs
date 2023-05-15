a907c27c5b1b5797e8b5783aa80277eeefdb455f
KmolYuan <pyslvs@gmail.com>
2023-05-15 11:53:15 +0800

    Simplify the macro.

--- four-bar-ui/src/app/proj/undo.rs
+++ four-bar-ui/src/app/proj/undo.rs
@@ -1,13 +1,6 @@
+use four_bar::FourBar;
+
 macro_rules! impl_undo_redo {
-    (@$(fn $method:ident($self:ident) { $ind:expr } => $($f:ident, $m:ident);+)+) => {$(
-        fn $method(&$self, state: &mut Self::State) {
-            *match $self.0 { $(Field::$f => state.$m()),+ } = $ind;
-        }
-    )+};
-    ($(fn $method:ident($self:ident) { $ind:expr })+) => {
-        impl_undo_redo!(@$(fn $method($self) { $ind } =>
-            P0x, p0x_mut; P0y, p0y_mut; A, a_mut; L1, l1_mut; L2, l2_mut; L3, l3_mut; L4, l4_mut; L5, l5_mut; G, g_mut)+);
-    };
     ($(fn $method:ident, $inv:ident)+) => {$(
         pub(crate) fn $method(&mut self, state: &mut D::State) {
             let Some(delta) = self.$method.pop() else { return };
@@ -24,55 +17,72 @@ pub(crate) trait Delta: Sized {
     fn delta(a: &Self::State, b: &Self::State) -> Option<Self>;
     fn undo(&self, state: &mut Self::State);
     fn redo(&self, state: &mut Self::State);
-    #[allow(unused_variables)]
-    fn require_merge(&self, rhs: &Self) -> bool {
-        false
-    }
-    #[allow(unused_variables)]
-    fn merge(&mut self, rhs: Self) {}
+    fn try_merge(&mut self, rhs: &Self) -> Option<()>;
 }
 
 #[derive(PartialEq)]
-enum Field {
-    P0x,
-    P0y,
-    A,
-    L1,
-    L2,
-    L3,
-    L4,
-    L5,
-    G,
+pub(crate) enum FourBarDelta {
+    P0x(f64, f64),
+    P0y(f64, f64),
+    A(f64, f64),
+    L1(f64, f64),
+    L2(f64, f64),
+    L3(f64, f64),
+    L4(f64, f64),
+    L5(f64, f64),
+    G(f64, f64),
+    Inv(bool, bool),
 }
 
-pub(crate) struct FourBarDelta(Field, f64, f64);
-
 impl Delta for FourBarDelta {
-    type State = four_bar::FourBar;
+    type State = FourBar;
 
     fn delta(a: &Self::State, b: &Self::State) -> Option<Self> {
         macro_rules! branch {
-            ($($f:ident, $m:ident);+) => {
+            ($($f:ident, $m:ident),+) => {
                 match (a, b) {
-                    $(_ if a.$m() != b.$m() => Some(Self(Field::$f, a.$m(), b.$m())),)+
+                    $(_ if a.$m() != b.$m() => Some(Self::$f(a.$m(), b.$m())),)+
                     _ => None,
                 }
             };
         }
-        branch!(P0x, p0x; P0y, p0y; A, a; L1, l1; L2, l2; L3, l3; L4, l4; L5, l5; G, g)
+        branch!(P0x, p0x, P0y, p0y, A, a, L1, l1, L2, l2, L3, l3, L4, l4, L5, l5, G, g, Inv, inv)
     }
 
-    impl_undo_redo! {
-        fn undo(self) { self.1 }
-        fn redo(self) { self.2 }
+    fn undo(&self, state: &mut Self::State) {
+        macro_rules! branch {
+            ($($f:ident, $m:ident),+) => {
+                match self { $(Self::$f(v, _) => *state.$m() = *v,)+ }
+            };
+        }
+        branch!(
+            P0x, p0x_mut, P0y, p0y_mut, A, a_mut, L1, l1_mut, L2, l2_mut, L3, l3_mut, L4, l4_mut,
+            L5, l5_mut, G, g_mut, Inv, inv_mut
+        );
     }
 
-    fn require_merge(&self, rhs: &Self) -> bool {
-        self.0 == rhs.0
+    fn redo(&self, state: &mut Self::State) {
+        macro_rules! branch {
+            ($($f:ident, $m:ident),+) => {
+                match self { $(Self::$f(_, v) => *state.$m() = *v,)+ }
+            };
+        }
+        branch!(
+            P0x, p0x_mut, P0y, p0y_mut, A, a_mut, L1, l1_mut, L2, l2_mut, L3, l3_mut, L4, l4_mut,
+            L5, l5_mut, G, g_mut, Inv, inv_mut
+        );
     }
 
-    fn merge(&mut self, rhs: Self) {
-        self.2 = rhs.2;
+    fn try_merge(&mut self, rhs: &Self) -> Option<()> {
+        macro_rules! branch {
+            ($($f:ident),+) => {
+                match (self, rhs) {
+                    $((Self::$f(_, lhs), Self::$f(_, rhs)) => Some(*lhs = *rhs),)+
+                    _ => None,
+                }
+            };
+        }
+        branch!(P0x, P0y, A, L1, L2, L3, L4, L5, G, Inv)
     }
 }
 
@@ -103,9 +113,12 @@ impl<D: Delta> Undo<D> {
             return;
         };
         let Some(delta) = D::delta(base, state) else { return };
-        if let Some(d) = self.undo.last_mut().filter(|d| d.require_merge(&delta)) {
-            d.merge(delta);
-        } else {
+        if self
+            .undo
+            .last_mut()
+            .and_then(|d| d.try_merge(&delta))
+            .is_none()
+        {
             self.undo.push(delta);
         }
         self.redo.clear();
