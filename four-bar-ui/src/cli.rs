7457ff29e1d3ebf959f87d1af07925de0ba251ef
KmolYuan <pyslvs@gmail.com>
2023-06-05 16:03:13 +0800

    Support to generate sphere codebook data.

--- four-bar-ui/src/cli.rs
+++ four-bar-ui/src/cli.rs
@@ -96,6 +96,9 @@ struct CbCfg {
     /// Generate for open curve
     #[clap(long)]
     is_open: bool,
+    /// Generate for spherical linkage
+    #[clap(long)]
+    sphere: bool,
     /// Number of data
     #[clap(long, default_value_t = cb::Cfg::new().size)]
     size: usize,
@@ -144,7 +147,15 @@ fn native(files: Vec<PathBuf>) {
 }
 
 fn codebook(cb: CbCfg) {
-    let CbCfg { mut file, is_open, size, res, harmonic, seed } = cb;
+    let CbCfg {
+        mut file,
+        is_open,
+        sphere,
+        size,
+        res,
+        harmonic,
+        seed,
+    } = cb;
     let ext = file.extension().and_then(std::ffi::OsStr::to_str);
     if !matches!(ext, Some("npz")) {
         file.set_extension("npz");
@@ -154,9 +165,13 @@ fn codebook(cb: CbCfg) {
     let t0 = std::time::Instant::now();
     let cfg = cb::Cfg { is_open, size, res, harmonic, seed: seed.into() };
     let pb = indicatif::ProgressBar::new(size as u64);
-    cb::FbCodebook::make_with(cfg, |n| pb.set_position(n as u64))
-        .write(std::fs::File::create(file).unwrap())
-        .unwrap();
+    let fs = std::fs::File::create(file).unwrap();
+    let callback = |n| pb.set_position(n as u64);
+    if sphere {
+        cb::SFbCodebook::make_with(cfg, callback).write(fs).unwrap();
+    } else {
+        cb::FbCodebook::make_with(cfg, callback).write(fs).unwrap();
+    }
     let t0 = t0.elapsed();
     pb.finish_and_clear();
     println!("Time spent: {t0:?}");
