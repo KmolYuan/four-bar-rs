6a510d702e616ae0fee9ea13981a9ba0f2227a22
KmolYuan <pyslvs@gmail.com>
2023-10-02 22:46:21 +0800

    Simplify the build script.

--- four-bar-ui/build.rs
+++ four-bar-ui/build.rs
@@ -6,12 +6,11 @@ fn main() {
     let profile = std::env::var("PROFILE").unwrap();
     let target_env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
     if target_env == "gnu" || target_env == "msvc" && profile == "release" {
-        let out = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
         let ico = image::open("assets/favicon.png").unwrap();
-        let ico_path = out.join("icon.ico");
-        ico.save(&ico_path).unwrap();
+        let path = std::env::var("OUT_DIR").unwrap() + std::path::MAIN_SEPARATOR_STR + "icon.ico";
+        ico.save(&path).unwrap();
         winres::WindowsResource::new()
-            .set_icon(ico_path.to_str().unwrap())
+            .set_icon(&path)
             .compile()
             .unwrap_or_default();
     }
