fb38d9e7d103b9a6180afca835d8927ad82950c4
KmolYuan <pyslvs@gmail.com>
2023-12-05 13:52:06 +0800

    Add MATLAB code to generate "fish" example.

---
+++ ssyn-examples/fish/fish.m
@@ -0,0 +1,15 @@
+theta = linspace(140, 215, 40);
+
+x = sind(65) * sind(-0.4685 * theta + 173.1994);
+y = -cosd(theta) .* cosd(-0.4685 * theta + 173.1994) + cosd(65) * sind(theta) .* sind(-0.4685 * theta + 173.1994);
+z = -sind(theta) .* cosd(-0.4685 * theta + 173.1994) - cosd(65) * cosd(theta) .* sind(-0.4685 * theta + 173.1994);
+writematrix(1000 * [x; y; z]', "fish.open.csv")
+
+figure
+plot3(x, y, z)
+% hold on
+% plot3([0, 1, 0, 0, -1, 0, 0], [0, 0, 1, 0, 0, -1, 0], [0, 0, 0, 1, 0, 0, -1], "ro")
+xlabel("X")
+ylabel("Y")
+zlabel("Z")
+axis equal
