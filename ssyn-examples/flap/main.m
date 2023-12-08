fb7ac5458f5c15004e6591d30172c925d8428c0b
KmolYuan <pyslvs@gmail.com>
2023-12-08 15:46:21 +0800

    Adjust "flap" example.

--- ssyn-examples/flap/main.m
+++ ssyn-examples/flap/main.m
@@ -1,9 +1,9 @@
 clear;clc;close all
 wt = linspace(0, 2*pi, 60);
-phi = 0.4 * sin(wt) + 0.5;
-psi = 0.36 * cos(wt) - 0.3;
-theta = 0.25 * cos(wt) + 0.2;
-p = [-10; 1; 0; 1];
+phi = 0.6 * sin(wt) + 0.5;
+psi = 0.8 * cos(wt) - 0.3;
+theta = 0.25 * cos(wt) + 0.25;
+p = [-0.06; 0.01; 0; 1];
 coord = zeros(60, 3);
 for i = 1:60
     m = DHm(0, pi/2, 0, phi(i)) * DHm(0, pi/2, 0.015, psi(i) - pi/2) * DHm(0, pi/2, 0, theta(i));
