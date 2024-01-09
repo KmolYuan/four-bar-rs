b8a96867b0abb69b20928155beb97f31d747ec5d
KmolYuan <pyslvs@gmail.com>
2024-01-09 13:50:23 +0800

    Upgrade egui.

--- four-bar-ui/src/app.rs
+++ four-bar-ui/src/app.rs
@@ -162,8 +162,7 @@ impl App {
     fn canvas(&mut self, ui: &mut Ui) {
         egui_plot::Plot::new("canvas")
             .data_aspect(1.)
-            .auto_bounds_x()
-            .auto_bounds_y()
+            .auto_bounds([true; 2].into())
             .legend(Default::default())
             .coordinates_formatter(egui_plot::Corner::LeftBottom, Default::default())
             .show(ui, |ui| {
