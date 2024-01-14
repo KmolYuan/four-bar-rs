ad9cdff6a39bd29560af451d3dcee4358b3afb29
KmolYuan <pyslvs@gmail.com>
2024-01-14 22:06:15 +0800

    Add more synthesis tasks.

--- four-bar/src/syn.rs
+++ four-bar/src/syn.rs
@@ -14,90 +14,17 @@
 //!     .solve()
 //!     .unwrap();
 //! ```
+pub use self::{motion::*, path::*};
 use crate::*;
-use std::{f64::consts::*, marker::PhantomData};
+use std::f64::consts::{PI, TAU};
 
-/// Boundary of the planar objective variables.
-pub const BOUND2D: &[[f64; 2]] = <NormFourBar as SynBound>::BOUND;
-/// Boundary of the spherical objective variables.
-pub const BOUND3D: &[[f64; 2]] = <SNormFourBar as SynBound>::BOUND;
+mod motion;
+mod path;
 
-/// Path generation task of planar four-bar linkage.
-pub type FbSyn = Syn<efd::D2, NormFourBar>;
-/// Path generation task of spherical four-bar linkage.
-pub type SFbSyn = Syn<efd::D3, SNormFourBar>;
-
-/// Path generation task of a mechanism.
-pub struct Syn<D: efd::EfdDim, M> {
-    /// Target coefficients
-    pub efd: efd::Efd<D>,
-    // Mode
-    mode: Mode,
-    // How many points need to be generated or compared
-    res: usize,
-    // Constrain the origin of the mechanism
-    origin: Option<efd::Coord<D>>,
-    // Constrain the scale of the mechanism
-    scale: Option<f64>,
-    // Marker of the mechanism
-    _marker: PhantomData<M>,
-}
-
-impl<D: efd::EfdDim, M> Syn<D, M> {
-    /// Create a new task from target curve. The harmonic number is selected
-    /// automatically.
-    ///
-    /// Return none if harmonic is zero or the curve is less than 1.
-    pub fn from_curve<C>(curve: C, mode: Mode) -> Self
-    where
-        C: efd::Curve<efd::Coord<D>>,
-    {
-        Self::from_efd(efd::Efd::from_curve(curve, mode.is_target_open()), mode)
-    }
-
-    /// Create a new task from target EFD coefficients.
-    pub fn from_efd(efd: efd::Efd<D>, mode: Mode) -> Self {
-        Self {
-            efd,
-            mode,
-            res: 180,
-            origin: None,
-            scale: None,
-            _marker: PhantomData,
-        }
-    }
-
-    /// Set the resolution during synthesis.
-    pub fn res(self, res: usize) -> Self {
-        assert!(res > 0);
-        Self { res, ..self }
-    }
-
-    /// Specify the mechanism is on origin and unit scale.
-    pub fn on_unit(self) -> Self {
-        self.origin(Default::default()).scale(1.)
-    }
-
-    /// Specify the origin of the mechanism.
-    pub fn origin(self, origin: efd::Coord<D>) -> Self {
-        Self { origin: Some(origin), ..self }
-    }
-
-    /// Specify the scale of the mechanism.
-    pub fn scale(self, scale: f64) -> Self {
-        assert!(scale > 0.);
-        Self { scale: Some(scale), ..self }
-    }
-
-    /// The harmonic used of target EFD.
-    pub fn harmonic(&self) -> usize {
-        self.efd.harmonic()
-    }
-}
-
-// Concat const slices, currently only support non-generic slices.
+// Concat const slices by their variable names, currently only support
+// non-generic slices.
 macro_rules! concat_slices {
-    ($a:ident $(, $b:ident)+) => {{
+    ($a:ident $(, $b:ident)+) => {&{
         let mut out = [$a[0]; { $a.len() $(+ $b.len())+ }];
         let mut i = 0;
         while i < $a.len() {
@@ -127,7 +54,7 @@ impl SynBound for NormFourBar {
         const K: f64 = 6.;
         const LNK: &[[f64; 2]] = &[[1. / K, K]; 4];
         const ANG: &[[f64; 2]] = &[[0., TAU]; 3];
-        &concat_slices!(LNK, ANG)
+        concat_slices!(LNK, ANG)
     };
 }
 
@@ -136,96 +63,8 @@ impl SynBound for SNormFourBar {
         const LNK: &[[f64; 2]] = &[[1e-4, PI]; 5];
         const GMA: &[[f64; 2]] = &[[0., PI]; 1];
         const ANG: &[[f64; 2]] = &[[0., TAU]; 2];
-        &concat_slices!(LNK, GMA, ANG)
+        concat_slices!(LNK, GMA, ANG)
     };
-}
-
-impl<D, M> mh::Bounded for Syn<D, M>
-where
-    D: efd::EfdDim + Sync + Send,
-    D::Trans: Sync + Send,
-    efd::Coord<D>: Sync + Send,
-    M: SynBound,
-{
-    #[inline]
-    fn bound(&self) -> &[[f64; 2]] {
-        if matches!(self.mode, Mode::Partial) {
-            M::BOUND
-        } else {
-            &M::BOUND[..M::BOUND.len() - 2]
-        }
-    }
-}
-
-impl<D, M> mh::ObjFunc for Syn<D, M>
-where
-    D: efd::EfdDim + Sync + Send,
-    D::Trans: Sync + Send,
-    efd::Coord<D>: efd::Distance + Sync + Send,
-    M: SynBound + fb::Statable + fb::FromVectorized + fb::Normalized<D> + fb::CurveGen<D>,
-    M::De: Default + Clone + fb::CurveGen<D> + Sync + Send + 'static,
-{
-    type Fitness = mh::Product<M::De, f64>;
-
-    fn fitness(&self, xs: &[f64]) -> Self::Fitness {
-        use efd::Distance as _;
-        #[cfg(feature = "rayon")]
-        use mh::rayon::prelude::*;
-        const INFEASIBLE: f64 = 1e10;
-        let infeasible = || mh::Product::new(INFEASIBLE, M::De::default());
-        let mut fb = M::from_vectorized_s1(&xs[..M::BOUND.len() - 2]).unwrap();
-        fb.set_to_planar_loop();
-        let (bound, states) =
-            fb.to_bound_states_filter(|a| a.check_mode(self.mode.is_result_open()));
-        let is_open = self.mode.is_target_open();
-        let f = |[t1, t2]: [f64; 2]| {
-            #[cfg(feature = "rayon")]
-            let iter = states.par_iter();
-            #[cfg(not(feature = "rayon"))]
-            let iter = states.iter();
-            iter.map(move |fb| (fb.curve_in(t1, t2, self.res), fb))
-                .filter(|(c, _)| c.len() > 2)
-                .map(|(c, fb)| {
-                    let efd = efd::Efd::<D>::from_curve_harmonic(c, is_open, self.efd.harmonic());
-                    let geo = efd.as_geo().to(self.efd.as_geo());
-                    let fb = fb.clone().trans_denorm(&geo);
-                    let o_err = match &self.origin {
-                        Some(o) => geo.trans().l2_norm(o),
-                        None => 0.,
-                    };
-                    let s_err = match self.scale {
-                        Some(s) => (geo.scale() - s).abs(),
-                        None => 0.,
-                    };
-                    let err = efd.distance(&self.efd).max(o_err).max(s_err);
-                    mh::Product::new(err, fb)
-                })
-        };
-        match self.mode {
-            Mode::Closed | Mode::Open => bound
-                .check_min()
-                .to_value()
-                .and_then(|t| f(t).min_by(|a, b| a.partial_cmp(b).unwrap()))
-                .unwrap_or_else(infeasible),
-            Mode::Partial => {
-                if !bound.is_valid() {
-                    return infeasible();
-                }
-                let bound = {
-                    let end = M::BOUND.len() - 1;
-                    fb::AngleBound::open_and_rev_at(xs[end], xs[end - 1])
-                };
-                #[cfg(feature = "rayon")]
-                let iter = bound.into_par_iter();
-                #[cfg(not(feature = "rayon"))]
-                let iter = bound.into_iter();
-                iter.filter_map(|b| b.check_min().to_value())
-                    .flat_map(f)
-                    .min_by(|a, b| a.partial_cmp(b).unwrap())
-                    .unwrap_or_else(infeasible)
-            }
-        }
-    }
 }
 
 /// Synthesis mode.
---
+++ four-bar/src/syn/path.rs
@@ -0,0 +1,168 @@
+use super::*;
+use std::marker::PhantomData;
+
+/// Boundary of the planar objective variables.
+pub const BOUND2D: &[[f64; 2]] = <NormFourBar as SynBound>::BOUND;
+/// Boundary of the spherical objective variables.
+pub const BOUND3D: &[[f64; 2]] = <SNormFourBar as SynBound>::BOUND;
+
+/// Path generation task of planar four-bar linkage.
+pub type FbSyn = PathSyn<efd::D2, NormFourBar>;
+/// Path generation task of spherical four-bar linkage.
+pub type SFbSyn = PathSyn<efd::D3, SNormFourBar>;
+
+/// Path generation of a mechanism `M`.
+pub struct PathSyn<D: efd::EfdDim, M> {
+    /// Target coefficients
+    pub efd: efd::Efd<D>,
+    // Mode
+    mode: Mode,
+    // How many points need to be generated or compared
+    res: usize,
+    // Constrain the origin of the mechanism
+    origin: Option<efd::Coord<D>>,
+    // Constrain the scale of the mechanism
+    scale: Option<f64>,
+    // Marker of the mechanism
+    _marker: PhantomData<M>,
+}
+
+impl<D: efd::EfdDim, M> PathSyn<D, M> {
+    /// Create a new task from target curve. The harmonic number is selected
+    /// automatically.
+    ///
+    /// Return none if harmonic is zero or the curve is less than 1.
+    pub fn from_curve<C>(curve: C, mode: Mode) -> Self
+    where
+        C: efd::Curve<efd::Coord<D>>,
+    {
+        Self::from_efd(efd::Efd::from_curve(curve, mode.is_target_open()), mode)
+    }
+
+    /// Create a new task from target EFD coefficients.
+    pub fn from_efd(efd: efd::Efd<D>, mode: Mode) -> Self {
+        Self {
+            efd,
+            mode,
+            res: 180,
+            origin: None,
+            scale: None,
+            _marker: PhantomData,
+        }
+    }
+
+    /// Set the resolution during synthesis.
+    pub fn res(self, res: usize) -> Self {
+        assert!(res > 0);
+        Self { res, ..self }
+    }
+
+    /// Specify the mechanism is on origin and unit scale.
+    pub fn on_unit(self) -> Self {
+        self.origin(Default::default()).scale(1.)
+    }
+
+    /// Specify the origin of the mechanism.
+    pub fn origin(self, origin: efd::Coord<D>) -> Self {
+        Self { origin: Some(origin), ..self }
+    }
+
+    /// Specify the scale of the mechanism.
+    pub fn scale(self, scale: f64) -> Self {
+        assert!(scale > 0.);
+        Self { scale: Some(scale), ..self }
+    }
+
+    /// The harmonic used of target EFD.
+    pub fn harmonic(&self) -> usize {
+        self.efd.harmonic()
+    }
+}
+
+impl<D, M> mh::Bounded for PathSyn<D, M>
+where
+    D: efd::EfdDim + Sync + Send,
+    D::Trans: Sync + Send,
+    efd::Coord<D>: Sync + Send,
+    M: SynBound,
+{
+    #[inline]
+    fn bound(&self) -> &[[f64; 2]] {
+        if matches!(self.mode, Mode::Partial) {
+            M::BOUND
+        } else {
+            &M::BOUND[..M::BOUND.len() - 2]
+        }
+    }
+}
+
+impl<D, M> mh::ObjFunc for PathSyn<D, M>
+where
+    D: efd::EfdDim + Sync + Send,
+    D::Trans: Sync + Send,
+    efd::Coord<D>: efd::Distance + Sync + Send,
+    M: SynBound + fb::Statable + fb::FromVectorized + fb::Normalized<D> + fb::CurveGen<D>,
+    M::De: Default + Clone + fb::CurveGen<D> + Sync + Send + 'static,
+{
+    type Fitness = mh::Product<M::De, f64>;
+
+    fn fitness(&self, xs: &[f64]) -> Self::Fitness {
+        use efd::Distance as _;
+        #[cfg(feature = "rayon")]
+        use mh::rayon::prelude::*;
+        const INFEASIBLE: f64 = 1e10;
+        let infeasible = || mh::Product::new(INFEASIBLE, M::De::default());
+        let mut fb = M::from_vectorized_s1(&xs[..M::BOUND.len() - 2]).unwrap();
+        fb.set_to_planar_loop();
+        let (bound, states) =
+            fb.to_bound_states_filter(|a| a.check_mode(self.mode.is_result_open()));
+        let is_open = self.mode.is_target_open();
+        let f = |[t1, t2]: [f64; 2]| {
+            #[cfg(feature = "rayon")]
+            let iter = states.par_iter();
+            #[cfg(not(feature = "rayon"))]
+            let iter = states.iter();
+            iter.map(move |fb| (fb.curve_in(t1, t2, self.res), fb))
+                .filter(|(c, _)| c.len() > 2)
+                .map(|(c, fb)| {
+                    let efd = efd::Efd::<D>::from_curve_harmonic(c, is_open, self.efd.harmonic());
+                    let geo = efd.as_geo().to(self.efd.as_geo());
+                    let fb = fb.clone().trans_denorm(&geo);
+                    let o_err = match &self.origin {
+                        Some(o) => geo.trans().l2_norm(o),
+                        None => 0.,
+                    };
+                    let s_err = match self.scale {
+                        Some(s) => (geo.scale() - s).abs(),
+                        None => 0.,
+                    };
+                    let err = efd.distance(&self.efd).max(o_err).max(s_err);
+                    mh::Product::new(err, fb)
+                })
+        };
+        match self.mode {
+            Mode::Closed | Mode::Open => bound
+                .check_min()
+                .to_value()
+                .and_then(|t| f(t).min_by(|a, b| a.partial_cmp(b).unwrap()))
+                .unwrap_or_else(infeasible),
+            Mode::Partial => {
+                if !bound.is_valid() {
+                    return infeasible();
+                }
+                let bound = {
+                    let end = M::BOUND.len() - 1;
+                    fb::AngleBound::open_and_rev_at(xs[end], xs[end - 1])
+                };
+                #[cfg(feature = "rayon")]
+                let iter = bound.into_par_iter();
+                #[cfg(not(feature = "rayon"))]
+                let iter = bound.into_iter();
+                iter.filter_map(|b| b.check_min().to_value())
+                    .flat_map(f)
+                    .min_by(|a, b| a.partial_cmp(b).unwrap())
+                    .unwrap_or_else(infeasible)
+            }
+        }
+    }
+}
