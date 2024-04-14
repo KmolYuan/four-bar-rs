564f43726d8d7a8bfd7490bc704c1cc2adb41bcf
KmolYuan <pyslvs@gmail.com>
2024-04-14 21:42:15 +0800

    Update iteration number

--- paper-ex.sh
+++ paper-ex.sh
@@ -13,7 +13,7 @@ fi
 FLAGS="--seed=0 -f"
 $FB syn $FLAGS --atlas=closed.npz test-fb/mcgarva.closed.csv
 $FB syn $FLAGS test-fb/yu2.closed.csv
-$FB syn $FLAGS test-fb/bow.open.ron
+$FB syn $FLAGS --gen=180 test-fb/bow.open.ron
 $FB syn $FLAGS test-fb/slice.partial.csv
 $FB syn $FLAGS test-fb/straight-line.partial.csv
 
