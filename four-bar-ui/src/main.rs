5a91f401ab69121a7ac17fba3c84aaaa6b8b10c5
KmolYuan <daan0014119@gmail.com>
2025-11-10 13:09:13 +0800

    Fix webview initialization

--- four-bar-ui/src/main.rs
+++ four-bar-ui/src/main.rs
@@ -14,9 +14,18 @@ fn main() {
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default();
     wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window().unwrap().document().unwrap();
+
+        use wasm_bindgen::JsCast as _;
+        let canvas = document
+            .get_element_by_id("app")
+            .unwrap()
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .unwrap();
+
         let opt = eframe::WebOptions::default();
         eframe::WebRunner::new()
-            .start("app", opt, app::App::create(Vec::new()))
+            .start(canvas, opt, app::App::create(Vec::new()))
             .await
             .expect("Startup failed");
     });
